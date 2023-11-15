917f351a07e1e62a6cacc5885b7644bec472eeaa
MdPiarulIslamAlif <mdpiarulislamalif7@gmail.com>
2023-11-15 20:25:57 +0600

    added a delete method on backend server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -49,6 +49,15 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
+
+    //delete method
+    app.delete('/carts/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
+    })
+
     app.post('/carts',async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
